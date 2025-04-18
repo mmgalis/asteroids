588938301354b9fe9bd58a013a4275bc8b4dce68
mmgalis <minagalis@gmail.com>
2025-04-18 22:22:54 +0200

    Drawing of triangle on screen

--- main.py
+++ main.py
@@ -1,28 +1,24 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
 	pygame.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
 	dt = 0
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	print("Starting Asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		pygame.Surface.fill(screen, (0,0,0))
+			
+		screen.fill("black")
+		player.draw(screen)
 		pygame.display.flip()
-		dt_ms = clock.tick(60)
-		dt = dt_ms/1000
-		
-	pygame.quit()
-      
+
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
